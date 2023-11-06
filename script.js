c8b2d163a8717b1bca5344eaafb5799f30c5e468
Lam Vo <lamvo.general@gmail.com>
2023-11-05 21:14:27 -0500

    Add caller for operations

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ const multiply = function (num1, num2) {
 const divide = function (num1, num2) {
   return num1 / num2;
 };
+
+const operate = function (num1, num2, operator) {
+  return operator(num1, num2);
+};
+
+console.log(operate(2, 3, add));
